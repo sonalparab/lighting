48c090f1f133dd139168035a7db298f6bece2726
sonalparab <sparab@stuy.edu>
2018-05-05 11:31:05 -0400

    a start

--- gmath.c
+++ gmath.c
@@ -9,38 +9,112 @@
 //lighting functions
 color get_lighting( double *normal, double *view, color alight, double light[2][3], double *areflect, double *dreflect, double *sreflect) {
   color i;
+
+  color a = calculate_ambient(alight,areflect);
+  color d = calculate_diffuse(light,dreflect,normal);
+  color s = calculate_specular(light,sreflect,view,normal);
+
+  i.red = a.red + d.red + s.red;
+  i.green = a.green + d.green + s.green;
+  i.blue = a.blue + d.blue + s.blue;
+
+  limit_color(&i);
+  
   return i;
 }
 
 color calculate_ambient(color alight, double *areflect ) {
   color a;
+  a.red = alight.red * areflect[0];
+  a.green = alight.green * areflect[1];
+  a.blue = alight.blue * areflect[2];
   return a;
 }
 
 color calculate_diffuse(double light[2][3], double *dreflect, double *normal ) {
   color d;
+ 
+  double prod = dot_product(light[0],normal);
+
+  d.red = light[1][0] * dreflect[0] * prod;
+  d.green = light[1][1] * dreflect[1] * prod;
+  d.blue = light[1][2] * dreflect[2] * prod;
+  
   return d;
 }
 
 color calculate_specular(double light[2][3], double *sreflect, double *view, double *normal ) {
-
   color s;
+
+  double scale = dot_product(normal,light[0]);
+  
+  double *t = (double *)malloc(3 * sizeof(double));
+  int i;
+  for(i = 0; i < 3; i++)
+    t[i] = scale * normal[i];
+
+  double *r = (double *)malloc(3 * sizeof(double));
+  for(i = 0; i < 3; i++)
+    r[i] = (2 * t[i]) - light[0][i];
+
+  double cosA = dot_product(r,view);
+
+  if(cosA < 0){
+    s.red = 0;
+    s.green = 0;
+    s.blue = 0;
+  }
+  else{
+    s.red = light[1][0] * sreflect[0] * pow(cosA,16);
+    s.green = light[1][1] * sreflect[1] * pow(cosA,16);
+    s.blue = light[1][2] * sreflect[2] * pow(cosA,16);
+  }
+
+  free(t);
+  free(r);
+   
   return s;
 }
 
 
 //limit each component of c to a max of 255
 void limit_color( color * c ) {
+  if(c->red > 255)
+    c->red = 255;
+  if(c->green > 255)
+    c->green = 255;
+  if(c->blue > 255)
+    c->blue = 255;
 }
 
 //vector functions
-//normalize vetor, should modify the parameter
+//normalize vector, should modify the parameter
 void normalize( double *vector ) {
+  double x = vector[0];
+  double y = vector[1];
+  double z = vector[2];
+
+  double mag = sqrt( (x * x) + (y * y) + (z * z));
+
+  vector[0] = x / mag;
+  vector[1] = y / mag;
+  vector[2] = z / mag;
+
 }
 
-//Return the dot porduct of a . b
+//Return the dot product of a . b
 double dot_product( double *a, double *b ) {
-  return 0;
+  double ax = a[0];
+  double ay = a[1];
+  double az = a[2];
+
+  double bx = b[0];
+  double by = b[1];
+  double bz = b[2];
+
+  double prod = (ax * bx) + (ay * by) + (az * bz);
+  
+  return prod;
 }
 
 double *calculate_normal(struct matrix *polygons, int i) {
